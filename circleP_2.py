71da46b9f7b630a15015991b873db2fbd5e5665c
R15pawan <ramamandhana935@gmail.com>
2024-09-27 20:18:08 +0530

    Add files via upload

---
+++ circleP_2.py
@@ -0,0 +1,23 @@
+import turtle
+turtle.Screen().bgcolor("green")
+board=turtle.Turtle()
+board.forward(100)
+
+board.left(60)
+board.forward(100)
+
+board.left(60)
+board.forward(100)
+
+board.left(60)
+board.forward(100)
+
+board.left(60)
+board.forward(100)
+
+board.left(60)
+board.forward(100)
+
+board.left(60)
+
+turtle.mainloop()
